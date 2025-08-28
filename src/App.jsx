4c1464a783217c6ea25dd7fb80eb715e554ef691
gul <gulmiracu@gmail.com>
2025-08-28 10:26:22 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import HealthBar from './components/HealthBar/HealthBar'
 import ChoicePrompts from './components/ChoicePrompts/ChoicePrompts'
 import Dialog from './components/Dialog/Dialog'
+import Cat from './components/Cat/Cat'
 
 function App() {
 
   return (
     <>
       <ChoicePrompts />
+      <Cat catState="hungry" />
       <Dialog />
     </>
   )
