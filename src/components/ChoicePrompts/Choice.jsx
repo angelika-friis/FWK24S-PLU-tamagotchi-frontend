878a5b9045a375ce1366cc88aab7c6a7b68ae1c1
angelika-friis <angelikafriis@gmail.com>
2025-09-01 12:05:29 +0200

    Remove local components (migrated to components repo) [#67]

--- src/components/ChoicePrompts/Choice.jsx
+++
@@ -1,9 +0,0 @@
-const Choice = ({ text, action }) => {
-    return (
-        <div className="choice" onClick={() => action(text)}>
-            {text}
-        </div>
-    );
-}
-
-export default Choice;
